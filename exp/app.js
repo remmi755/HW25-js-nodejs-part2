3851ede673d55c6374113b4623ded2e05043f63a
remmi755 <90457721+remmi755@users.noreply.github.com>
2022-02-10 14:49:27 +0300

    add homework

---
+++ exp/app.js
@@ -0,0 +1,32 @@
+const createError = require('http-errors');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const url = require('url');
+const port = 3000;
+
+app.use( function (req, res, next) {
+  if(req.url === '/home') {
+    res.send('Home');
+  } else {
+    next()
+  }
+});
+
+app.use('/forbidden', function (req, res) {
+  const urlData = url.parse(req.url, true);
+  if ( urlData.search === '?secret=true') {
+    res.end('Access approved')
+  } else {
+    res.end('Access denied')
+  }
+})
+
+app.listen(port)
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+module.exports = router;
+module.exports = app;
